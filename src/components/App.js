12a437559dd6e2ca748b58130f2f5774717b40b6
shinobi9 <shinobi9c@outlook.com>
2021-07-22 20:50:56 +0800

    add react-router"

--- src/components/App.js
+++
@@ -1,9 +0,0 @@
-import Borderlands2Calculator from './Borderlands2Calculator';
-
-function App() {
-  return (
-    <Borderlands2Calculator />
-  );
-}
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
-
-import App from './components/App';
+import { Router, Route } from 'react-router';
+import { createHashHistory } from 'history';
+import Borderlands2Calculator from './components/Borderlands2Calculator';
+import Welcome from './components/Welcome';
 
 ReactDOM.render(
-  <App />,
+  <Router history={createHashHistory()}>
+    <Route exact path="/" component={Welcome} />
+    <Route path="/borderlands2-calculator" component={Borderlands2Calculator} />
+  </Router>,
   document.getElementById('root')
 );
