2c99a046eda8bb2eaafb23a88fd37af2d6309b4f
shinobi9 <shinobi9c@outlook.com>
2021-07-23 20:07:10 +0800

    fix event with a simple way

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,193 +1,186 @@
 import React from 'react';
 import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import {makeStyles, useTheme} from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import { ListItemText, ListItem, IconButton, Typography, Toolbar, Drawer, Collapse } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import {ListItemText, ListItem, IconButton, Typography, Toolbar, Drawer, Collapse} from '@material-ui/core';
+import {Link} from 'react-router-dom';
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-  },
-  appBar: {
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  hide: {
-    display: 'none',
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    zIndex: 0,//
-    paddingTop: 60,//
-    width: drawerWidth,
-  },
-  drawerHeader: {
-    display: 'flex',
-    alignItems: 'center',
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    marginLeft: -drawerWidth,
-  },
-  contentShift: {
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginLeft: 0,
-  },
-  link: {
-    textDecoration: "none",
-    color: "black"
-  },
-  lists: {
-    backgroundColor: theme.palette.background.paper,
-    marginTop: theme.spacing(1),
-  },
-  nested: {
-    paddingLeft: theme.spacing(4),
-  },
+    root: {
+        display: 'flex',
+    },
+    appBar: {
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    hide: {
+        display: 'none',
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        zIndex: 0,//
+        paddingTop: 60,//
+        width: drawerWidth,
+    },
+    drawerHeader: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+        justifyContent: 'flex-end',
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        marginLeft: -drawerWidth,
+    },
+    contentShift: {
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginLeft: 0,
+    },
+    link: {
+        textDecoration: "none",
+        color: "black"
+    },
+    lists: {
+        backgroundColor: theme.palette.background.paper,
+        marginTop: theme.spacing(1),
+    },
+    nested: {
+        paddingLeft: theme.spacing(4),
+    },
 }));
 
 
-
-
-
 export default function Welcome(props) {
-  const classes = useStyles();
-  const theme = useTheme();
+    const classes = useStyles();
+    const theme = useTheme();
 
-  const [open, setOpen] = React.useState(false);
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-
-
-  const [borderlands2, setBorderlands2] = React.useState(true);
-  const [payday2, setPayday2] = React.useState(true);
-
-  const handleExpandClick = (e) => {
-    let map = new Map([
-      ["borderlands2", () => setBorderlands2(!borderlands2)],
-      ["payday2", () => setPayday2(!payday2)],
-    ])
-    console.debug(e)
-    let { name, value } = e.target
-    console.debug(`${name} => ${value}`)
-    let func = map.get(name)
-    if (func) {
-      func()
-    }
-  };
+    const [open, setOpen] = React.useState(false);
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
 
 
+    const [borderlands2, setBorderlands2] = React.useState(false);
+    const [payday2, setPayday2] = React.useState(false);
 
-  return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
-        })}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            className={clsx(classes.menuButton, open && classes.hide)}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap>
-            Game Tools
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        className={classes.drawer}
-        variant="persistent"
-        anchor="left"
-        open={open}
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-      >
-        <div className={classes.drawerHeader}>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-          </IconButton>
+    const handleExpandClick = (e,menu) => {
+        let map = new Map([
+            ["borderlands2", () => setBorderlands2(!borderlands2)],
+            ["payday2", () => setPayday2(!payday2)],
+        ])
+        let func = map.get(menu)
+        if (func) {
+            func()
+        }
+    };
+
+
+    return (
+        <div className={classes.root}>
+            <CssBaseline/>
+            <AppBar
+                position="fixed"
+                className={clsx(classes.appBar, {
+                    [classes.appBarShift]: open,
+                })}
+            >
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        className={clsx(classes.menuButton, open && classes.hide)}
+                    >
+                        <MenuIcon/>
+                    </IconButton>
+                    <Typography variant="h6" noWrap>
+                        Game Tools
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                className={classes.drawer}
+                variant="persistent"
+                anchor="left"
+                open={open}
+                classes={{
+                    paper: classes.drawerPaper,
+                }}
+            >
+                <div className={classes.drawerHeader}>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === 'ltr' ? <ChevronLeftIcon/> : <ChevronRightIcon/>}
+                    </IconButton>
+                </div>
+                <Link className={classes.link} to="#">
+                    <ListItem button key={"borderlands2"} onClick={(e) => handleExpandClick(e,"borderlands2")}>
+                        <ListItemText primary={"borderlands2"}/>
+                    </ListItem>
+                </Link>
+                <Collapse in={borderlands2} timeout="auto" unmountOnExit>
+                    <Link className={classes.link} to="/borderlands2/calculator">
+                        <ListItem button key={"borderlands2-calculator"}>
+                            <ListItemText primary={"· 压血计算器"}/>
+                        </ListItem>
+                    </Link>
+                </Collapse>
+                <Link className={classes.link} to="#">
+                    <ListItem button key={"payday2"} onClick={(e) => handleExpandClick(e,"payday2")}>
+                        <ListItemText primary={"payday2"}/>
+                    </ListItem>
+                </Link>
+                <Collapse in={payday2} timeout="auto" unmountOnExit>
+                    <Link className={classes.link} to="/payday2/bigoil">
+                        <ListItem button key={"payday2-bigoil"}>
+                            <ListItemText primary={"· 大油引擎"}/>
+                        </ListItem>
+                    </Link>
+                </Collapse>
+            </Drawer>
+            <main
+                className={clsx(classes.content, {
+                    [classes.contentShift]: open,
+                })}
+            >
+                <div className={classes.drawerHeader}/>
+                {props.children}
+            </main>
         </div>
-        <Link className={classes.link} to="#" >
-          <ListItem button key={"borderlands2"} name={"borderlands2"} onClick={(e) => handleExpandClick(e)}>
-            <ListItemText primary={"borderlands2"} />
-          </ListItem>
-        </Link>
-        <Collapse in={borderlands2} timeout="auto" unmountOnExit>
-          <Link className={classes.link} to="/borderlands2/calculator" >
-            <ListItem button key={"borderlands2-calculator"}>
-              <ListItemText primary={"· 压血计算器"} />
-            </ListItem>
-          </Link>
-        </Collapse>
-        <Link className={classes.link} to="#" >
-          <ListItem button key={"payday2"} name={"payday2"} onClick={(e) => handleExpandClick(e)}>
-            <ListItemText primary={"payday2"} />
-          </ListItem>
-        </Link>
-        <Collapse in={payday2} timeout="auto" unmountOnExit>
-          <Link className={classes.link} to="/payday2/bigoil" >
-            <ListItem button key={"payday2-bigoil"}>
-              <ListItemText primary={"· 大油引擎"} />
-            </ListItem>
-          </Link>
-        </Collapse>
-      </Drawer>
-      <main
-        className={clsx(classes.content, {
-          [classes.contentShift]: open,
-        })}
-      >
-        <div className={classes.drawerHeader} />
-        {props.children}
-      </main>
-    </div >
-  );
+    );
 }
--- src/components/payday2/BigOil.js
+++ src/components/payday2/BigOil.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { withStyles } from '@material-ui/core/styles';
-import { Chip, Grid, Radio, RadioGroup, FormControlLabel, FormControl, FormLabel, Divider } from '@material-ui/core';
-import { chooseEngineBy } from '../../misc/BigOilSelector'
+import {withStyles} from '@material-ui/core/styles';
+import {Chip, Grid, Radio, RadioGroup, FormControlLabel, FormControl, FormLabel, Divider} from '@material-ui/core';
+import {chooseEngineBy} from '../../misc/BigOilSelector'
+
 const useStyles = theme => ({
     root: {
         display: 'flex',
@@ -25,30 +26,33 @@ class BigOil extends React.Component {
             pressure: "???",
         }
     }
+
     handleChange(event) {
         console.debug(event.target)
-        let { name, value } = event.target
+        let {name, value} = event.target
         console.debug(`${name} => ${value}`)
         this.setState({
             [name]: value
         })
     }
+
     render() {
-        const { classes } = this.props;
-        const { gas, tubeCount, pressure } = this.state
+        const {classes} = this.props;
+        const {gas, tubeCount, pressure} = this.state
         const result = chooseEngineBy(gas, tubeCount, pressure)
 
         return (
             <div>
                 <Grid container spacing={3}>
                     <Grid item xs={6} sm={3}>
-                        <FormControl component="fieldset" >
+                        <FormControl component="fieldset">
                             <FormLabel component="legend">gas</FormLabel>
-                            <RadioGroup aria-label="gas" name="gas" value={this.state.gas} onChange={e => this.handleChange(e)}>
-                                <FormControlLabel value="???" control={<Radio />} label="? ? ?" />
-                                <FormControlLabel value="Nitrogen" control={<Radio />} label="Nitrogen" />
-                                <FormControlLabel value="Deuterium" control={<Radio />} label="Deuterium" />
-                                <FormControlLabel value="Helium" control={<Radio />} label="Helium" />
+                            <RadioGroup aria-label="gas" name="gas" value={this.state.gas}
+                                        onChange={e => this.handleChange(e)}>
+                                <FormControlLabel value="???" control={<Radio/>} label="? ? ?"/>
+                                <FormControlLabel value="Nitrogen" control={<Radio/>} label="Nitrogen"/>
+                                <FormControlLabel value="Deuterium" control={<Radio/>} label="Deuterium"/>
+                                <FormControlLabel value="Helium" control={<Radio/>} label="Helium"/>
                             </RadioGroup>
                         </FormControl>
                     </Grid>
@@ -57,11 +61,12 @@ class BigOil extends React.Component {
                     <Grid item xs={6} sm={3}>
                         <FormControl component="fieldset">
                             <FormLabel component="legend">tube count</FormLabel>
-                            <RadioGroup aria-label="tubeCount" name="tubeCount" value={this.state.tubeCount} onChange={e => this.handleChange(e)}>
-                                <FormControlLabel value="???" control={<Radio />} label="? ? ?" />
-                                <FormControlLabel value="H" control={<Radio />} label="H" />
-                                <FormControlLabel value="2xH" control={<Radio />} label="2 × H" />
-                                <FormControlLabel value="3xH" control={<Radio />} label="3 × H" />
+                            <RadioGroup aria-label="tubeCount" name="tubeCount" value={this.state.tubeCount}
+                                        onChange={e => this.handleChange(e)}>
+                                <FormControlLabel value="???" control={<Radio/>} label="? ? ?"/>
+                                <FormControlLabel value="H" control={<Radio/>} label="H"/>
+                                <FormControlLabel value="2xH" control={<Radio/>} label="2 × H"/>
+                                <FormControlLabel value="3xH" control={<Radio/>} label="3 × H"/>
                             </RadioGroup>
                         </FormControl>
                     </Grid>
@@ -70,36 +75,37 @@ class BigOil extends React.Component {
                     <Grid item xs={6} sm={3}>
                         <FormControl component="fieldset">
                             <FormLabel component="legend">pressure</FormLabel>
-                            <RadioGroup aria-label="pressure" name="pressure" value={this.state.pressure} onChange={e => this.handleChange(e)}>
-                                <FormControlLabel value="???" control={<Radio />} label="? ? ?" />
-                                <FormControlLabel value=">=5783" control={<Radio />} label="≥ 5783" />
-                                <FormControlLabel value="<=5812" control={<Radio />} label="≤ 5812" />
+                            <RadioGroup aria-label="pressure" name="pressure" value={this.state.pressure}
+                                        onChange={e => this.handleChange(e)}>
+                                <FormControlLabel value="???" control={<Radio/>} label="? ? ?"/>
+                                <FormControlLabel value=">=5783" control={<Radio/>} label="≥ 5783"/>
+                                <FormControlLabel value="<=5812" control={<Radio/>} label="≤ 5812"/>
                             </RadioGroup>
                         </FormControl>
                     </Grid>
                     <Grid item xs={12}>
-                        <Divider />
+                        <Divider/>
                     </Grid>
                     <Grid item xs={12}>
                         <div className={classes.root}>
                             {
                                 result.map(engine => (
-                                    <Chip
-                                        size="small"
-                                        label={engine}
-                                        clickable
-                                        color="primary"
-                                    />
-                                )
+                                        <Chip
+                                            size="small"
+                                            label={engine}
+                                            clickable
+                                            color="primary"
+                                            key={engine}
+                                        />
+                                    )
                                 )
                             }
                         </div>
                     </Grid>
                 </Grid>
-            </div >
+            </div>
         )
     }
-
 
 
 }
